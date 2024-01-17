7a444278cd3779b38ecfe46bf8b7a0590066a9b8
prabath1998 <prabhathudayanga1998@gmail.com>
2024-01-17 19:50:08 +0530

    init

---
+++ routes/web.php
@@ -0,0 +1,29 @@
+<?php
+
+use App\Events\MessageNotification;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+
+Route::get('/event', function(){
+    event(new MessageNotification('First broadcast!'));
+});
+
+
+Route::get('/listen', function(){
+    return view('listen');
+});
